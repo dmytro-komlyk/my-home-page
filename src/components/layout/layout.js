49ba269ab447c7f7a22449208ab46b0ab024806a
Dmitriy <dima.komlyk@gmail.com>
2023-05-17 11:28:45 +0300

    Implemented mui styling

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -1,5 +1,9 @@
 import * as React from "react"
 // import { useStaticQuery, graphql } from "gatsby"
+import {
+  Box
+} from '@mui/material';
+import { StyledEngineProvider } from '@mui/material/styles';
 
 import Header from "../header/header"
 import Footer from "../footer/footer"
@@ -8,13 +12,13 @@ import * as styles from "./layout.module.scss"
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <Header siteTitle='Title' />
-      <div className={styles.wrapper}>
+    <StyledEngineProvider injectFirst>
+      <Box className={styles.wrapper}>
+        <Header siteTitle='Title'/>
         <main className={styles.main}>{children}</main>
-      </div>
-      <Footer />
-    </>
+        <Footer />
+      </Box>
+    </StyledEngineProvider>
   )
 }
   
