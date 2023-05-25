42451c725d024cc13d8c9ce0f5b4ff0d4615ba1b
Dmitriy <dima.komlyk@gmail.com>
2023-05-25 19:08:25 +0300

    Added check null

--- src/components/card-skill/card-skill.js
+++ src/components/card-skill/card-skill.js
@@ -11,16 +11,15 @@ import {
 import * as styles from "./card-skill.module.scss";
 
 const CardSkill = ({ data }) => {
-  const { title, stackList } = data;
 
   return (
     <Card className={styles.cardSkill}>
       <CardActionArea>
         <CardContent>
-          <Typography className={styles.cardSkillTitle} gutterBottom variant="h5" component="h3">{ title }</Typography>
+          <Typography className={styles.cardSkillTitle} gutterBottom variant="h5" component="h3">{ data?.title }</Typography>
           <Divider />
           <Stack className={styles.cardSkillStack} direction="row" spacing={1}>
-            { stackList?.split(" ").map((skill, id) => (
+            { data?.stackList?.split(" ").map((skill, id) => (
               <Typography key={id} gutterBottom variant="body1" component="div">{ skill }</Typography>
             )) }
           </Stack>
